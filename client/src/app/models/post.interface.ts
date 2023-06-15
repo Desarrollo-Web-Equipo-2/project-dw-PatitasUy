f750b1f6bbaa28bc6a1e289ffb7f050068b74ab9
alfonsoLeandro <leandroalfonsoporley@gmail.com>
2023-06-15 00:49:08 -0300

    Adds Post interface.

---
+++ client/src/app/models/post.interface.ts
@@ -0,0 +1,10 @@
+export interface Post {
+    title: string;
+    photoUrls: string[];
+    description: string;
+    age: string;
+    gender: string;
+    type: string;
+    size: string;
+    location: string;
+}
