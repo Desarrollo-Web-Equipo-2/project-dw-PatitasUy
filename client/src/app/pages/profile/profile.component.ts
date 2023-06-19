26111e2402a479903cb6259ba9af36217a1deebf
NicoLorenzo1 <88661584+NicoLorenzo1@users.noreply.github.com>
2023-06-19 15:05:53 -0300

    Buttons select styles added

--- client/src/app/pages/profile/profile.component.ts
+++ client/src/app/pages/profile/profile.component.ts
@@ -7,33 +7,35 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
 
-  publications: Array<any> = [
-    {
-      imageURL: "https://c.files.bbci.co.uk/48DD/production/_107435681_perro1.jpg",
-      title: "Headline 6",
-      state: "activo",
-    },
-    {
-      imageURL: "https://c.files.bbci.co.uk/48DD/production/_107435681_perro1.jpg",
-      title: "Headline 6",
-      state: "activo",
-    },
-    {
-      imageURL: "https://c.files.bbci.co.uk/48DD/production/_107435681_perro1.jpg",
-      title: "Headline 6",
-      state: "activo",
-    },
-    {
-      imageURL: "https://c.files.bbci.co.uk/48DD/production/_107435681_perro1.jpg",
-      title: "Headline 6",
-      state: "activo",
-    },
-    {
-      imageURL: "https://c.files.bbci.co.uk/48DD/production/_107435681_perro1.jpg",
-      title: "Headline 6",
-      state: "activo",
-    },
-  ];
+  publications: Array<any> = [];
 
+  activeButton: string = 'publications';
+  setActiveButton(button: string) {
+    this.activeButton = button;
+  }
 
+  selectPublications(button: string) {
+
+    this.setActiveButton(button);
+
+    if (button === 'myPublications') {
+      this.publications = [{
+        imageURL: "https://c.files.bbci.co.uk/48DD/production/_107435681_perro1.jpg",
+        title: "HeadlinePublications",
+        state: "activo",
+      },
+      {
+        imageURL: "https://c.files.bbci.co.uk/48DD/production/_107435681_perro1.jpg",
+        title: "HeadlineFavoritos",
+        state: "activo",
+      }]
+    }
+    else if (button === 'myFavorites') {
+      this.publications = [{
+        imageURL: "https://c.files.bbci.co.uk/48DD/production/_107435681_perro1.jpg",
+        title: "HeadlineFavoritos",
+        state: "activo",
+      }]
+    }
+  }
 }
