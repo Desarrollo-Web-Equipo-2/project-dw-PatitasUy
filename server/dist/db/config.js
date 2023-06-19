8ffa5061a0067082dafedd28a9e58837f5be203e
Dana Castroman <danacastroman@gmail.com>
2023-06-19 18:47:52 -0300

    Fixed .gitignore to ignore .vscode and /dist

--- server/dist/db/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-const db = new sequelize_1.Sequelize('railway', 'root', '4pokgDSDKOx5uZlkSENd', {
-    host: 'mysql://root:4pokgDSDKOx5uZlkSENd@containers-us-west-151.railway.app:6915/railway',
-    dialect: 'mysql'
-});
-exports.default = db;
-//# sourceMappingURL=config.js.map
