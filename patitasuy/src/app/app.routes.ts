66e23cb64bc67e866fead039f96a0177b96ff1d7
Juan Cabrera <j.cabrera@correo.ucu.edu.uy>
2023-06-22 22:56:18 -0300

    Add basic pages structure

--- patitasuy/src/app/app.routes.ts
+++ patitasuy/src/app/app.routes.ts
@@ -3,11 +3,39 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('./pages/profile/profile.page').then( m => m.ProfilePage)
+  },
+  {
+    path: 'post',
+    loadComponent: () => import('./pages/post/post.page').then( m => m.PostPage)
+  },
+  {
+    path: 'messages',
+    loadComponent: () => import('./pages/messages/messages.page').then( m => m.MessagesPage)
+  },
+  {
+    path: 'details',
+    loadComponent: () => import('./pages/details/details.page').then( m => m.DetailsPage)
+  },
+  {
+    path: 'chat',
+    loadComponent: () => import('./pages/chat/chat.page').then( m => m.ChatPage)
   },
 ];
