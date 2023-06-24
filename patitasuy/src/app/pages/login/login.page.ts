07b36c2ac67d4e82d2bddbd3c30176cf26580441
Dana Castroman <danacastroman@gmail.com>
2023-06-24 15:26:56 -0300

    login page view

--- patitasuy/src/app/pages/login/login.page.ts
+++ patitasuy/src/app/pages/login/login.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ContentChild, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  email: string = '';
+  password: string = '';
+
+  hidePassword: boolean = true;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  login() { }
 
 }
