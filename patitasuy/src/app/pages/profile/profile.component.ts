ad597245357ca170c8b883b1cf862ee861bc5c5d
NicoLorenzo1 <88661584+NicoLorenzo1@users.noreply.github.com>
2023-06-27 01:43:59 -0300

    add profile styless

--- patitasuy/src/app/pages/profile/profile.component.ts
+++ patitasuy/src/app/pages/profile/profile.component.ts
@@ -13,14 +13,143 @@ export class ProfileComponent {
   publications: Post[] = [];
 
   constructor(private readonly postService: PostsService) {
-    this.postService.getAllPublicationsPerson(9).subscribe({
-      next: (res) => {
-        this.publications = res;
-      },
-      error: (error) => {
-        console.log(error);
-      }
-    });
+    /*
+        this.postService.getAllPublicationsPerson(9).subscribe({
+          next: (res) => {
+            this.publications = res;
+          },
+          error: (error) => {
+            console.log(error);
+          }
+        });
+        */
+
+    this.publications = [{
+      id: 0,
+      title: 'Post Publicacion 1',
+      photoUrls: [
+        'https://images.pexels.com/photos/3687770/pexels-photo-3687770.jpeg'
+      ],
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid autem cum eaque id incidunt iure libero, molestias nulla obcaecati odit provident quaerat quasi quidem reprehenderit repudiandae sequi temporibus velit voluptates. texto jaja hola buenas pedo culo caca pis',
+      age: '1 year',
+      gender: 'male',
+      type: 'dog',
+      size: 'medium',
+      location: 'Montevideo, Uruguay',
+      state: "Activo"
+    },
+    {
+      id: 0,
+      title: 'Post Publicacion 1',
+      photoUrls: [
+        'https://images.pexels.com/photos/3687770/pexels-photo-3687770.jpeg'
+      ],
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid autem cum eaque id incidunt iure libero, molestias nulla obcaecati odit provident quaerat quasi quidem reprehenderit repudiandae sequi temporibus velit voluptates. texto jaja hola buenas pedo culo caca pis',
+      age: '1 year',
+      gender: 'male',
+      type: 'dog',
+      size: 'medium',
+      location: 'Montevideo, Uruguay',
+      state: "Activo"
+    },
+    {
+      id: 0,
+      title: 'Post Publicacion 1',
+      photoUrls: [
+        'https://images.pexels.com/photos/3687770/pexels-photo-3687770.jpeg'
+      ],
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid autem cum eaque id incidunt iure libero, molestias nulla obcaecati odit provident quaerat quasi quidem reprehenderit repudiandae sequi temporibus velit voluptates. texto jaja hola buenas pedo culo caca pis',
+      age: '1 year',
+      gender: 'male',
+      type: 'dog',
+      size: 'medium',
+      location: 'Montevideo, Uruguay',
+      state: "Activo"
+    },
+    {
+      id: 0,
+      title: 'Post Publicacion 1',
+      photoUrls: [
+        'https://images.pexels.com/photos/3687770/pexels-photo-3687770.jpeg'
+      ],
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid autem cum eaque id incidunt iure libero, molestias nulla obcaecati odit provident quaerat quasi quidem reprehenderit repudiandae sequi temporibus velit voluptates. texto jaja hola buenas pedo culo caca pis',
+      age: '1 year',
+      gender: 'male',
+      type: 'dog',
+      size: 'medium',
+      location: 'Montevideo, Uruguay',
+      state: "Activo"
+    },
+    {
+      id: 0,
+      title: 'Post Publicacion 1',
+      photoUrls: [
+        'https://images.pexels.com/photos/3687770/pexels-photo-3687770.jpeg'
+      ],
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid autem cum eaque id incidunt iure libero, molestias nulla obcaecati odit provident quaerat quasi quidem reprehenderit repudiandae sequi temporibus velit voluptates. texto jaja hola buenas pedo culo caca pis',
+      age: '1 year',
+      gender: 'male',
+      type: 'dog',
+      size: 'medium',
+      location: 'Montevideo, Uruguay',
+      state: "Activo"
+    },
+    {
+      id: 0,
+      title: 'Post Publicacion 1',
+      photoUrls: [
+        'https://images.pexels.com/photos/3687770/pexels-photo-3687770.jpeg'
+      ],
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid autem cum eaque id incidunt iure libero, molestias nulla obcaecati odit provident quaerat quasi quidem reprehenderit repudiandae sequi temporibus velit voluptates. texto jaja hola buenas pedo culo caca pis',
+      age: '1 year',
+      gender: 'male',
+      type: 'dog',
+      size: 'medium',
+      location: 'Montevideo, Uruguay',
+      state: "Activo"
+    },
+    {
+      id: 0,
+      title: 'Post Publicacion 1',
+      photoUrls: [
+        'https://images.pexels.com/photos/3687770/pexels-photo-3687770.jpeg'
+      ],
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid autem cum eaque id incidunt iure libero, molestias nulla obcaecati odit provident quaerat quasi quidem reprehenderit repudiandae sequi temporibus velit voluptates. texto jaja hola buenas pedo culo caca pis',
+      age: '1 year',
+      gender: 'male',
+      type: 'dog',
+      size: 'medium',
+      location: 'Montevideo, Uruguay',
+      state: "Activo"
+    },
+    {
+      id: 0,
+      title: 'Post Publicacion 1',
+      photoUrls: [
+        'https://images.pexels.com/photos/3687770/pexels-photo-3687770.jpeg'
+      ],
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid autem cum eaque id incidunt iure libero, molestias nulla obcaecati odit provident quaerat quasi quidem reprehenderit repudiandae sequi temporibus velit voluptates. texto jaja hola buenas pedo culo caca pis',
+      age: '1 year',
+      gender: 'male',
+      type: 'dog',
+      size: 'medium',
+      location: 'Montevideo, Uruguay',
+      state: "Activo"
+    },
+    {
+      id: 1,
+      title: 'Post Publicacion 2',
+      photoUrls: [
+        'https://images.pexels.com/photos/3687770/pexels-photo-3687770.jpeg'
+      ],
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid autem cum eaque id incidunt iure libero, molestias nulla obcaecati odit provident quaerat quasi quidem reprehenderit repudiandae sequi temporibus velit voluptates. texto jaja hola buenas pedo culo caca pis',
+      age: '1 year',
+      gender: 'male',
+      type: 'dog',
+      size: 'medium',
+      location: 'Montevideo, Uruguay',
+      state: "Activo"
+    }]
   }
 
   selectPublications(event: any) {
