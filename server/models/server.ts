66589be78899df6032ddf3b4c478c9bde28578c4
Dana Castroman <danacastroman@gmail.com>
2023-06-27 17:50:04 -0300

    Upload image - backend

--- server/models/server.ts
+++ server/models/server.ts
@@ -2,25 +2,29 @@ import express, { Application } from 'express';
 import postRoutes from '../routes/post.routes';
 import userRoutes from '../routes/user.routes';
 import authRoutes from '../routes/auth.routes';
+import uploadRoutes from '../routes/uploads.routes';
 import cors from 'cors';
 import db from '../db/config';
+import fileUpload from 'express-fileupload';
 
 class Server {
   private app: Application;
   private port: string | number;
   private apiPaths = {
     posts: '/api/posts',
-    users: '/api/users'
+    users: '/api/users',
+    uploads: '/api/uploads'
   }
   private authPath = {
     auth: '/api/auth'
   }
 
+
   constructor() {
     this.app = express();
     this.port = process.env.SERVER_PORT || 3000
 
-    this.dbConnection();
+
 
     this.middlewares();
 
@@ -41,12 +45,20 @@ class Server {
     this.app.use(cors());
 
     this.app.use(express.json());
+
+    this.app.use(fileUpload({
+      useTempFiles: true,
+      tempFileDir: '/tmp/',
+      createParentPath: true
+    }));
   }
 
   routes() {
     this.app.use(this.authPath.auth, authRoutes);
     this.app.use(this.apiPaths.posts, postRoutes);
     this.app.use(this.apiPaths.users, userRoutes);
+    this.app.use(this.apiPaths.uploads, uploadRoutes);
+
   }
 
   listen() {
