9039cbdf0069c2bbc5da274ab6593ee7a6d18701
YOGURSIO <yogursio@gmail.com>
2023-06-28 19:59:46 -0300

    Message + chat adaptation

--- patitasuy/src/app/pages/chat/chat.component.ts
+++ patitasuy/src/app/pages/chat/chat.component.ts
@@ -1,14 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Chat } from 'src/app/interfaces/chat';
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss'],
 })
-export class ChatComponent  implements OnInit {
+export class ChatComponent implements OnInit {
 
-  constructor() { }
+  messageId!: number;
+  constructor(private route: ActivatedRoute, private msgService: MessageService) { }
 
-  ngOnInit() {}
+  chat: Chat | undefined;
 
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.messageId = params['id'];
+      this.msgService.getChatById(Number(this.messageId)).subscribe(values => {
+        this.chat = values;
+      });
+    });
+  }
 }
