b2dc9db75f869d11183a9134caa87dcf50a3ad2f
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2023-06-28 16:59:08 -0300

    Adds getCurrentUser and setCurrentUser to UserService.

--- patitasuy/src/app/services/user/user.service.ts
+++ patitasuy/src/app/services/user/user.service.ts
@@ -6,6 +6,7 @@ import { environmentProd } from 'src/app/environments/environment.prod';
 import { environment } from 'src/app/environments/environment';
 import { User } from 'src/app/interfaces/user';
 import { UserResponse } from 'src/app/interfaces/response';
+import { Preferences } from '@capacitor/preferences';
 
 @Injectable({
   providedIn: 'root'
@@ -27,5 +28,13 @@ export class UserService {
     const body = user;
 
     return this.http.post<UserResponse>(url, body);
+  }
+
+  setCurrentUser(user: User) {
+      return Preferences.set({ key: 'user', value: JSON.stringify(user) });
+  }
+
+  getCurrentUser() {
+      return Preferences.get({ key: 'user' });
   }
 }
