f83caff4ebe081b7a8d1f0c1442842827f23686b
[Franco Barlocco] <[titobarlocco52@gmail.com]>
2023-06-28 18:58:37 -0300

    create filter ionic

--- patitasuy/src/app/app.module.ts
+++ patitasuy/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -43,9 +45,9 @@ import { ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    NgIf,
-    ComponentsModule,
-    NgForOf,
+      NgIf,
+      ComponentsModule,
+      NgForOf,
     ReactiveFormsModule
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
--- patitasuy/src/app/pages/home/home.component.ts
+++ patitasuy/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Post } from 'src/app/models/post.interface';
 import { PostsService } from 'src/app/services/posts.service';
 
@@ -9,12 +10,42 @@ import { PostsService } from 'src/app/services/posts.service';
 })
 export class HomeComponent  implements OnInit {
   allPosts: Post[] | undefined;
-
-  constructor(private postsService: PostsService) { }
+  myForm: FormGroup;
+  filteredPosts?: Post[]; 
+  constructor(private postsService: PostsService,private formBuilder: FormBuilder) { 
+    this.myForm = this.formBuilder.group({
+      age: '',
+      sex: '',
+      size: '',
+    });
+  }
 
   ngOnInit() {
     this.postsService.getAll().subscribe(posts => {
       this.allPosts = posts;
+      this.filteredPosts = posts;
     });
+  }
+  submitForm() {
+    const ageValue = this.myForm.value.age;
+    const sizeValue = this.myForm.value.size;
+    const sexValue = this.myForm.value.sex;
+  
+    this.allPosts = this.filteredPosts?.filter((post: Post) => {
+      const ageMatch = ageValue === '' || post.age === ageValue;
+      const sizeMatch = sizeValue === '' || post.size === sizeValue;
+      const sexMatch = sexValue === '' || post.gender === sexValue;
+      
+      return ageMatch && sizeMatch && sexMatch;
+    });
+    console.log(this.filteredPosts)
+  }
+
+  ageFilter(){
+    const ageValue = this.myForm.value.age;
+    switch(ageValue){
+      
+    }
+    
   }
 }
