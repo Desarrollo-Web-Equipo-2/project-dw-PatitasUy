fed7adb92898ea3a7af7311000909fe1d229cc78
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2023-06-28 20:29:15 -0300

    Adds setFavorite endpoint

--- server/routes/post.routes.ts
+++ server/routes/post.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { getPosts, getFavoritePosts, getIsFavorite } from "../controllers/post.controller";
+import { getPosts, getFavoritePosts, getIsFavorite, setFavorite } from "../controllers/post.controller";
+import { existsPost, existsUser } from '../middlewares/validate-existance';
 const router = Router();
 
 router.get('/', getPosts);
 router.get('/user/:userId', getFavoritePosts);
-router.get('/isFavorite/post/:postId/user/:userId', getIsFavorite);
+router.get('/isFavorite/post/:postId/user/:userId', [existsPost, existsUser], getIsFavorite);
+
+router.put('/setFavorite/post/:postId/user/:userId', [existsPost, existsUser], setFavorite);
 
 export default router;
