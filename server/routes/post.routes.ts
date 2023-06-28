7e284bd0e7de19b8cc2f1ba1ca1c06761a9f044f
NicoLorenzo1 <88661584+NicoLorenzo1@users.noreply.github.com>
2023-06-28 17:17:11 -0300

    Revert change of delete endpoint

--- server/routes/post.routes.ts
+++ server/routes/post.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getPosts, getFavoritePosts } from "../controllers/post.controller";
+import { getPosts, getFavoritePosts, getIsFavorite } from "../controllers/post.controller";
 const router = Router();
 
 router.get('/', getPosts);
 router.get('/user/:userId', getFavoritePosts);
+router.get('/isFavorite/post/:postId/user/:userId', getIsFavorite);
 
 export default router;
