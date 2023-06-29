399104054ada3ea601d1b9dba9f65e871b6f6dac
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2023-06-29 00:35:09 -0300

    Adds ApiService to make requests with the JWT token included

---
+++ patitasuy/src/app/services/api/api.service.ts
@@ -0,0 +1,83 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Preferences } from '@capacitor/preferences';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ApiService {
+
+    private readonly apiUrl = environment.apiUrl;
+
+    constructor(private http: HttpClient) {
+    }
+
+    get<T>(path: string, options?: any): Promise<any> {
+        return new Promise((resolve, reject) => {
+            Preferences.get({ key: 'Authorization' }).then((token) => {
+                if (!token) {
+                    reject({ msg: 'Invalid token! Please login again' });
+                    return;
+                }
+                this.http.get<T>(`${this.apiUrl}/${path}`, {
+                    ...options,
+                    headers: new HttpHeaders().append('Authorization', token.value!)
+                }).subscribe({
+                    next: (resp) => {
+                        resolve(resp);
+                    },
+                    error: (err) => {
+                        reject(err);
+                    }
+                })
+            });
+        });
+    }
+
+    put<T>(path: string, body?: any, options?: any): Promise<any> {
+        return new Promise((resolve, reject) => {
+            Preferences.get({ key: 'Authorization' }).then((token) => {
+                if (!token) {
+                    reject({ msg: 'Invalid token! Please login again' });
+                    return;
+                }
+                this.http.put<T>(`${this.apiUrl}/${path}`, body, {
+                    ...options,
+                    headers: new HttpHeaders().append('Authorization', token.value!)
+                }).subscribe({
+                    next: (resp) => {
+                        resolve(resp);
+                    },
+                    error: (err) => {
+                        reject(err);
+                    }
+                })
+            });
+        });
+    }
+
+    post<T>(path: string, body?: any, options?: any): Promise<any> {
+        return new Promise((resolve, reject) => {
+            Preferences.get({ key: 'Authorization' }).then((token) => {
+                if (!token) {
+                    reject({ msg: 'Invalid token! Please login again' });
+                    return;
+                }
+                this.http.post<T>(`${this.apiUrl}/${path}`, body, {
+                    ...options,
+                    headers: new HttpHeaders().append('Authorization', token.value!)
+                }).subscribe({
+                    next: (resp) => {
+                        resolve(resp);
+                    },
+                    error: (err) => {
+                        reject(err);
+                    }
+                })
+            });
+        });
+    }
+
+
+}
