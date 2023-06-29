cfa897bab3348ce8e14d5d41a8a5c06efec6f2fe
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2023-06-29 00:42:26 -0300

    Adds isValidToken method to auth service.

--- patitasuy/src/app/services/auth.service.ts
+++ patitasuy/src/app/services/auth.service.ts
@@ -2,25 +2,35 @@ import { Injectable } from '@angular/core';
 import { tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { Preferences } from '@capacitor/preferences';
+import { ApiService } from './api/api.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
 
-  private apiUrl: string = environment.apiUrl || 'http://localhost:3000/api'
+    private apiRoute = 'auth';
+    private readonly apiUrl = environment.apiUrl + '/' + this.apiRoute;
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient,
+                private apiService: ApiService) {
+    }
 
-  login(email: string, password: string) {
-    const body = { email, password };
-    return this.http.post<any>(`${this.apiUrl}/auth/login`, body).pipe(
-      tap((resp: any) => {
-        if(resp.token) {
-          localStorage.setItem('Authorization', resp.token);
-        }
-      }
-    ));
-  }
+    login(email: string, password: string) {
+        const body = { email, password };
+        return this.http.post<any>(`${this.apiUrl}/login`, body).pipe(
+            tap((resp: any) => {
+                    if (resp.token) {
+                        Preferences.set({ key: 'Authorization', value: resp.token });
+                    }
+                }
+            ));
+    }
+
+    isValidToken() {
+        return this.apiService.get<boolean>(this.apiRoute + '/isValidToken');
+    }
+
 }
 
