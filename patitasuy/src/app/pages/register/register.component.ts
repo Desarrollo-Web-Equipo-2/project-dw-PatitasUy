3ccaf6a56b0bd03b904c0d9b93a5e5242caef43b
Cdogonza <gonzalo.ucu@correo.ucu.edu.uy>
2023-06-29 03:11:02 -0300

    new branch with user register

--- patitasuy/src/app/pages/register/register.component.ts
+++ patitasuy/src/app/pages/register/register.component.ts
@@ -1,14 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/interfaces/user';
+import { UserService } from 'src/app/services/user/user.service';
+import { environment } from './../../../environments/environment.prod';
+import { Component} from '@angular/core';
+import {FormControl,  FormGroup, Validators} from '@angular/forms';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent  implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  hidePassword: boolean = true;
+  user: any;
+  formRegister = new FormGroup({
+    name: new FormControl(''),
+    surname: new FormControl(''),
+    email: new FormControl('', Validators.email),
+    password: new FormControl('')
+  });
+  private apiUrl = environment;
+  
 
-  ngOnInit() {}
+
+  constructor(private userService: UserService,private router: Router) { 
+    
+  }
+
+  register(user:User):Observable<any> {
+    const url = `${this.apiUrl}/register`;
+    return this.userService.createUser(user)
+  };
+
+  
 
 }
