0d6a19fe14560f17a9715a076de6cf10993a1321
NicoLorenzo1 <88661584+NicoLorenzo1@users.noreply.github.com>
2023-07-02 19:08:21 -0300

    State en my publications added

--- patitasuy/src/app/components/post-card/post-card.component.ts
+++ patitasuy/src/app/components/post-card/post-card.component.ts
@@ -1,16 +1,29 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Post } from 'src/app/interfaces/post.interface';
+import { PostsService } from 'src/app/services/posts.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-post-card',
   templateUrl: './post-card.component.html',
   styleUrls: ['./post-card.component.scss'],
 })
-export class PostCardComponent  implements OnInit {
+export class PostCardComponent implements OnInit {
 
   @Input() post!: Post;
 
-  constructor() { }
+  constructor(public userService: UserService) { }
 
-  ngOnInit() {}
+  getCurrentUserId() {
+    let id = 0;
+    this.userService.getCurrentUser().subscribe((userData) => {
+      if (userData?.user_id) {
+        id = userData.user_id;
+      }
+    }
+    )
+    return id;
+  }
+
+  ngOnInit() { }
 }
--- patitasuy/src/app/services/posts.service.ts
+++ patitasuy/src/app/services/posts.service.ts
@@ -8,9 +8,7 @@ import { environment } from "../../environments/environment";
     providedIn: 'root'
 })
 export class PostsService {
-
     private readonly apiUrl = environment.apiUrl + '/posts';
-
 
     constructor(private http: HttpClient) {
     }
